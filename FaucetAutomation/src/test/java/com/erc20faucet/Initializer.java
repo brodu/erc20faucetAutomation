f9867c6c211c6155e745b4ceca50919be5fa1a63
brodu <brodu0110@gmail.com>
2022-04-05 22:40:39 +0300

    Update Initializer.java

--- FaucetAutomation/src/test/java/com/erc20faucet/Initializer.java
+++ FaucetAutomation/src/test/java/com/erc20faucet/Initializer.java
@@ -1,11 +1,18 @@
 package com.erc20faucet;
 
-
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Initializer {
     public static ChromeDriver driver;
     public ChromeOptions options;
+    public final String password = new String("12345678a");
+
+    public void waitUntilVisible(WebElement element, int time) throws RuntimeException, InterruptedException {
+        while (element.isDisplayed() != true){
+            Thread.sleep(time);
+        }
+    }
 
 }
